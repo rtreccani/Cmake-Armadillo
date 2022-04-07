c5e8f5ce770c9c47bc07a18709375a4fc13088bd
rtreccani <raphael.treccanichinelli@gmail.com>
2022-04-07 20:16:06 +0100

    Added armadillo example code to prove it runs correctly

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
-#include <stdio.h>
+#include <iostream>
 #include <armadillo>
 
+using namespace std;
+using namespace arma;
+
 int main(){
-    printf("hello world!\n");
+    mat A(4,5, fill::randu);
+    mat B(4,5, fill::randu);
+    cout << A*B.t() <<endl;
+    return 0;
 }
